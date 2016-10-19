b381a94237505504b7131eab4fefe32d76df3aff
Aura <Aura@auraPC>
2016-10-19 21:12:57 +0200

    version 2

--- Lyra/src/main/Main.java
+++ Lyra/src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Bot bot = new Bot("irc.esper.net", "#fourcannon", "SweetLyra", "Duskie");
+		Bot bot = new Bot("", "", "", "");
 		
 	}
 
