0a9207bc0f49d2f2f8e8bbabd1c66890c6debb1d
Aura <Aura@auraPC>
2016-10-30 16:20:01 +0100

    db

--- Lyra/src/main/Main.java
+++ Lyra/src/main/Main.java
@@ -7,10 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String irc = "";
-		String channel = "";
-		String botName = "";
-		String botAdmin = "";
+		String irc = "irc.esper.net";
+		String channel = "#fourcannon";
+		String botName = "SweetLyra";
+		String botAdmin = "Duskie";
 		
 		
 		
